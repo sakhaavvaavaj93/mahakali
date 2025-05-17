da11aa0ad06fd4f67ea200be7abb078ddf9f5381
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-05-18 03:31:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Energy_level100")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Mallus_Street")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/KURUK_SHE_TRA")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", ""))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "")
